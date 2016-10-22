b1b913bf961d0fc52c0670c1dba9d465732dc304
penny_xiaojuan <penny_xiaojuan@outlook.com>
2016-10-22 23:08:17 +0800

    Read data from file

--- app/src/main/java/com/example/penny/filepersistencetest/MainActivity.java
+++ app/src/main/java/com/example/penny/filepersistencetest/MainActivity.java
@@ -3,22 +3,39 @@ package com.example.penny.filepersistencetest;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+/**
+ * 在creat()方法中调用load()方法来读取文件中存储的文本内容，如果读到的内容不为null，
+ * 就调用EditText的setText()方法将内容填充到EditText里，并调用setSelection方法将
+ * 输入光标移动到文本的末尾位置以便继续输入，然后弹出一句还原成功的提示。
+ */
 public class MainActivity extends Activity {
     private EditText edit;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edit = (EditText) findViewById(R.id.edit);
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)){
+            edit.setText(inputText);
+            edit.setSelection(inputText.length());
+            Toast.makeText(this, "Restoring succeeded",Toast.LENGTH_SHORT).show();
+        }
     }
+
+
     @Override
     public void onDestroy(){
         super.onDestroy();
@@ -43,5 +60,31 @@ public class MainActivity extends Activity {
                 e.printStackTrace();
             }
         }
+    }
+
+
+    public  String load(){
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try{
+            in = openFileInput ("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null){
+                content.append(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(reader!= null){
+                try{
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
     }
 }
